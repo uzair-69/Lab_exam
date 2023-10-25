fcb6fdd9137c4708b0c9d641c297537dfdc0024f
uzair-69 <muhammaduzair.bssef21@iba-suk.edu.pk>
2023-10-25 14:00:02 +0500

    ..

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+function Data(){
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+    var description = document.getElementById("Description").value;
+    var address = document.getElementById("Address").value;
+
+    console.log(name);
+    console.log(email);
+    console.log(phone);
+    console.log(description);
+    console.log(address);
+    
+}
